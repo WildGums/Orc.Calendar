9d440086703158c04291ace928286ea6700c83f7
Geert van Horrik <geert@catenalogic.com>
2026-03-19 14:30:48 +0100

    Update

--- src/Orc.Calendar.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Calendar.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,19 +1,18 @@
-﻿namespace Orc.Calendar.Tests
+﻿namespace Orc.Calendar.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcCalendar();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcCalendar();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
